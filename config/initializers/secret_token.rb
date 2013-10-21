cd986bfb0b144f6159acf1496f91cfb1ca14ccb0
Phil <p.doise@gmail.com>
2013-10-21 14:32:13 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MDS3Buidler::Application.config.secret_key_base = '8ae2d389adcb237909500c6e08c1383b528e4276b510e8198c2267d1e19214c41f7936fae92d9b11a2ee4ce2246f2c215bda2a1f8af57390f58c430fd41dfd75'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MDS3Buidler::Application.config.session_store :cookie_store, key: '_MDS3Buidler_session'
