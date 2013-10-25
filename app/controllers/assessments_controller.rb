3a6ebad12ff1941da07d8a0144070be1fd34dad9
Phil <p.doise@gmail.com>
2013-10-25 13:17:02 -0600

    add i/o to file sysytem

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -5,14 +5,20 @@ class AssessmentsController < ApplicationController
 	end
 
   def create
-    @foo = "foo"
-    respond_to do |format|
-       send_data(@foo, :type => "application/xml", :filename=>"MDS Assessment.xml", :disposition => 'attachment')
+    builder = Nokogiri::XML::Builder.new do |xml|
+    xml.root {
+      xml.products {
+        xml.widget {
+          xml.id_ "10"
+          xml.name "Awesome widget"
+        }
+      }
+    }
     end
+    send_data(builder.to_xml, :type => "application/xml", :filename=>"MDS.xml", :disposition => 'attachment')
   end
 
   private
-
 
   def get_correct_assessment_type
     discharge = MdsDischarge.new
