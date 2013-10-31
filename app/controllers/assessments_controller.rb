6c93eb258de210719dde3300f2c24070567035c4
Phil <p.doise@gmail.com>
2013-10-31 09:26:52 -0600

    tweak

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,19 +1,12 @@
 class AssessmentsController < ApplicationController
 
 	def new
-    @assessment_fields = get_correct_assessment_type.map{|field| field.constantize.new }
+    @@assessment = get_correct_assessment_type
+    @assessment_fields = @@assessment.fields.map{|field| field.constantize.new }
 	end
 
   def create
-    @assessment = Assessment.new
-    send_data(@assessment.pull_xml_values.to_xml, :type => "application/xml", :filename=>"MDS.xml", :disposition => 'attachment')
-  end
-
-  def get_correct_assessment
-    discharge = MdsDischarge.new
-    case params[:type]
-    when "Discharge" then return discharge
-    end
+    send_data(@@assessment.pull_xml_values.to_xml, :type => "application/xml", :filename=>"MDS.xml", :disposition => 'attachment')
   end
 
   private
@@ -21,7 +14,7 @@ class AssessmentsController < ApplicationController
   def get_correct_assessment_type
     discharge = MdsDischarge.new
     case params[:type]
-    when "Discharge" then return discharge.fields
+    when "Discharge" then return discharge
     end
   end
 
