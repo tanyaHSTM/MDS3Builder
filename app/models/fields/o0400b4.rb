7edb3b4f36282c3dc352dd4f084f1a4db3c14835
pdoise <p.doise@gmail.com>
2014-02-06 12:35:36 -0700

    fixed special treatments fields

--- app/models/fields/o0400b4.rb
+++ app/models/fields/o0400b4.rb
@@ -5,11 +5,19 @@ class O0400b4
     @title = "Special Treaments, Procedures and Programs"
     @name = "Occupational Therapy - Number of days this therapy was administered for at least 15 minutes 
              a day in the last 7 days (O0400b4)"
-    @field_type = TEXT
+    @field_type = DROPDOWN
     @node = "O0400B4"     
 
     @options = []
-    @options << FieldOption.new("")
+    @options << FieldOption.new("^", "NA")
+    @options << FieldOption.new("0", "None")
+    @options << FieldOption.new("1", "1 Day")
+    @options << FieldOption.new("2", "2 Days")
+    @options << FieldOption.new("3", "3 Days")
+    @options << FieldOption.new("4", "4 Days")
+    @options << FieldOption.new("5", "5 Days")
+    @options << FieldOption.new("6", "6 Days")
+    @options << FieldOption.new("7", "7 Days")
   end
 
   def set_values_for_type(klass)
--- app/models/fields/o0400c4.rb
+++ app/models/fields/o0400c4.rb
@@ -4,11 +4,19 @@ class O0400c4
   def initialize
     @name = "Physical Therapy - Number of days this therapy was administered for at least 15 minutes 
              a day in the last 7 days (O0400c4)"
-    @field_type = TEXT
+    @field_type = DROPDOWN
     @node = "O0400C4"     
 
     @options = []
-    @options << FieldOption.new("")
+    @options << FieldOption.new("^", "NA")
+    @options << FieldOption.new("0", "None")
+    @options << FieldOption.new("1", "1 Day")
+    @options << FieldOption.new("2", "2 Days")
+    @options << FieldOption.new("3", "3 Days")
+    @options << FieldOption.new("4", "4 Days")
+    @options << FieldOption.new("5", "5 Days")
+    @options << FieldOption.new("6", "6 Days")
+    @options << FieldOption.new("7", "7 Days")
   end
 
   def set_values_for_type(klass)
