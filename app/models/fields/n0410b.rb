abf7c0839398cd06c7c775e6374f4a778a27b24a
pdoise <p.doise@gmail.com>
2014-02-06 11:48:27 -0700

    interger valitdation fix

---
+++ app/models/fields/n0410b.rb
@@ -0,0 +1,25 @@
+class N0410b
+  attr_reader :options, :name, :field_type, :node
+
+  def initialize
+    @name = "Antianxiety (N0410b)"
+    @field_type = DROPDOWN
+    @node = "N0410B"     
+
+    @options = []
+    @options << FieldOption.new("^", "NA")
+    @options << FieldOption.new("0", "None")
+    @options << FieldOption.new("1", "1 Day")
+    @options << FieldOption.new("2", "2 Days")
+    @options << FieldOption.new("3", "3 Days")
+    @options << FieldOption.new("4", "4 Days")
+    @options << FieldOption.new("5", "5 Days")
+    @options << FieldOption.new("6", "6 Days")
+    @options << FieldOption.new("7", "7 Days")
+  end
+  
+  def set_values_for_type(klass)
+    return "^"
+  end
+  
+end
