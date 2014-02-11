fe9f942b0314f028967591db176d661d7f47aeb2
pdoise <p.doise@gmail.com>
2014-02-11 10:51:55 -0700

    fix for radio buttons

--- app/models/fields/b1200.rb
+++ app/models/fields/b1200.rb
@@ -2,7 +2,7 @@ class B1200
   attr_reader :options, :name, :field_type, :node
 
   def initialize
-    @name = "Corrective Lenses (Contacts, glasses or maginifying glass) used in completing B100, Vision. (B1200)"
+    @name = "Corrective Lenses (Contacts, glasses or maginifying glass) used in completing B0100, Vision. (B1200)"
     @field_type = RADIO
     @node = "B1200" 
 
