ee273e1e2eef10d448be73a29fbcd787091839b6
pdoise <p.doise@gmail.com>
2014-02-13 15:49:38 -0700

    inactivation logic

--- app/models/inactivation_of_admission.rb
+++ app/models/inactivation_of_admission.rb
@@ -7,7 +7,7 @@ class InactivationOfAdmission < Assessment
   end
 
   def fields
-    super + %w(  X0150 X0200a X0200c X0300 X0400 X0500 X0600a X0600b X0600c X0600d X0600f X0700a X0700c )
+     %w( ItmSbstCd StateCd FacId A0050 X0150 X0200a X0200c X0300 X0400 X0500 X0600a X0600b X0600c X0600d X0600f X0700a X0700c )
   end
 
 end
