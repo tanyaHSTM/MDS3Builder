e9d3ba1fb0a21058e7704632e70d79be09fe3ade
pdoise <p.doise@gmail.com>
2014-02-20 11:48:53 -0700

    progress

--- app/contexts/apply_qp_parameter_context.rb
+++ app/contexts/apply_qp_parameter_context.rb
@@ -9,18 +9,18 @@ class ApplyQpParameterContext
     @assessment, @qp_name, @pos_or_neg = assessment, qp_name, pos_or_neg
 
     @assessment.extend Qps::AttributeSetter
-    @assessment.extend qp_primary_attribute_module(@qp_name, @pos_or_neg)
+    @assessment.extend qp_attribute_module(@qp_name, @pos_or_neg)
   end
 
   def call
     return unless @qp_name.present?
     @assessment.set_all_to_nil
-    @assessment.apply_requested_primary_qp_attributes(@pos_or_neg)
+    @assessment.apply_requested_qp_attributes(@pos_or_neg)
   end
 
   private
 
-  def qp_primary_attribute_module(qp_name, pos_or_neg)
+  def qp_attribute_module(qp_name, pos_or_neg)
     "Qps::#{@qp_name}".constantize
   end
 
