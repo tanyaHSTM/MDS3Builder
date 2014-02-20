da93486f3d92b0a6e73edfc46271e122cd78852c
pdoise <p.doise@gmail.com>
2014-02-20 09:09:50 -0700

    qp017 pos

--- app/roles/qps/attribute_setter.rb
+++ app/roles/qps/attribute_setter.rb
@@ -1,194 +1,35 @@
 module Qps::AttributeSetter
 
   def set_all_to_nil
-    #nil_B0200; nil_B0300; nil_B1000; nil_B1200; nil_E0200A; nil_E0800; nil_G0110A1; nil_G0110B1; nil_G0110E1; nil_G0110G1;
-    #nil_G0110H1; nil_G0110I1; nil_G0400A; nil_G0400B; nil_H0100A; nil_H0300; nil_I2300; nil_I2500; nil_J1550C; nil_K0300;
-   # nil_K0510B2; nil_L0200A; nil_L0200B; nil_L0200C; nil_L0200D; nil_L0200E; nil_L0200F; nil_L0200G; nil_L0200Z; nil_M0210;
-   nil_M0300B1
+  	nil_text(text_klasses_to_nil)
+    nil_radio_and_dropdown(radio_and_dropdown_klasses_to_nil)
   end
 
-  def nil_M0300B1
-  	field = klasses.detect {|x| x.node == "M0300B1"}
-  	field_option = field.options.each{|x| x.value = "^"; x.selected = true} 
+  def radio_and_dropdown_klasses_to_nil
+    array = [ "B0200", "B0300", "B1000", "B1200", "E0200A", "E0800", "G0110A1", "G0110B1", "G0110E1", "G0110G1", "G0110H1", "G0110I1",
+              "G0400A", "G0400B", "H0100A", "H0300", "I2300", "I2500", "J1550C", "K0300", "K0510B2", "L0200A", "L0200B", "L0200C", 
+              "L0200D", "L0200E", "L0200F", "L0200G", "L0200Z", "M0210", "N0410A", "N0410B", "N0410C", "N0410D", "N0410E", "N0410F",
+              "N0410G", "O0400B4", "O0400C4", "P0100B", "P0100C", "P0100D", "P0100E", "P0100F", "P0100G",]
   end
 
-  def nil_B0200
-    field = klasses.detect {|x| x.node == "B0200"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
+  def text_klasses_to_nil
+  	array = ["M0300A", "M0300B1", "M0300B2", "M0300C1", "M0300C2", "M0300D1", "M0300D2", "M0300E1", "M0300E2", "M0300F1", "M0300F2",
+  	         "M0300G1", "M0300G2"]
   end
 
-  def nil_B0300
-    field = klasses.detect {|x| x.node == "B0300"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
+  def nil_text(text_klasses_to_nil)
+  	  text_klasses_to_nil.each do |klass_to_nil|
+  	  field = klasses.detect {|x| x.node == klass_to_nil}
+  	  field_option = field.options.each{|x| x.value = "^"; x.selected = true} 
+    end
   end
 
-  def nil_B1000
-    field = klasses.detect {|x| x.node == "B1000"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
+  def nil_radio_and_dropdown(radio_and_dropdown_klasses_to_nil)
+  	radio_and_dropdown_klasses_to_nil.each do |klass_to_nil|
+       field = klasses.detect {|x| x.node == klass_to_nil}
+       field_option = field.options.detect{|x| x.value == "^"} 
+       field_option.selected = true
+    end
   end
 
-  def nil_B1200
-    field = klasses.detect {|x| x.node == "B1200"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_E0200A
-    field = klasses.detect {|x| x.node == "E0200A"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_E0800
-    field = klasses.detect {|x| x.node == "E0800"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_G0110A1
-    field = klasses.detect {|x| x.node == "G0110A1"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_G0110B1
-    field = klasses.detect {|x| x.node == "G0110B1"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_G0110E1
-    field = klasses.detect {|x| x.node == "G0110E1"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_G0110G1
-    field = klasses.detect {|x| x.node == "G0110G1"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_G0110H1
-    field = klasses.detect {|x| x.node == "G0110H1"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_G0110I1
-    field = klasses.detect {|x| x.node == "G0110I1"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_G0400A
-    field = klasses.detect {|x| x.node == "G0400A"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_G0400B
-    field = klasses.detect {|x| x.node == "G0400B"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_H0100A
-    field = klasses.detect {|x| x.node == "H0100A"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_H0300
-    field = klasses.detect {|x| x.node == "H0300"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_I2300
-    field = klasses.detect {|x| x.node == "I2300"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_I2500
-    field = klasses.detect {|x| x.node == "I2500"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_J1550C
-    field = klasses.detect {|x| x.node == "J1550C"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_K0300
-    field = klasses.detect {|x| x.node == "K0300"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_K0510B2
-    field = klasses.detect {|x| x.node == "K0510B2"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_L0200A
-    field = klasses.detect {|x| x.node == "L0200A"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_L0200B
-    field = klasses.detect {|x| x.node == "L0200B"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_L0200C
-    field = klasses.detect {|x| x.node == "L0200C"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_L0200D
-    field = klasses.detect {|x| x.node == "L0200D"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_L0200E
-    field = klasses.detect {|x| x.node == "L0200E"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_L0200F
-    field = klasses.detect {|x| x.node == "L0200F"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_L0200G
-    field = klasses.detect {|x| x.node == "L0200G"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_L0200Z
-    field = klasses.detect {|x| x.node == "L0200Z"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
-
-  def nil_M0210
-    field = klasses.detect {|x| x.node == "M0210"}
-    field_option = field.options.detect{|x| x.value == "^"} 
-    field_option.selected = true
-  end
 end
--- app/roles/qps/qp017.rb
+++ app/roles/qps/qp017.rb
@@ -1,7 +1,43 @@
 module Qps::Qp017
+  #:g0110a1, :g0110b1, :g0110h1, :g0110i1]
 
   def apply_requested_primary_qp_attributes
+    set_G0110A1_pos
+    set_G0110B1_pos
+    set_G0110H1_pos
+    set_G0110I1_pos
+  end
 
+  def set_G0110A1_pos
+    field = klasses.detect {|x| x.node == "G0110A1"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "0"} 
+    field_option.selected = true
+  end
+
+  def set_G0110B1_pos
+    field = klasses.detect {|x| x.node == "G0110B1"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "0"} 
+    field_option.selected = true
+  end
+
+  def set_G0110H1_pos
+    field = klasses.detect {|x| x.node == "G0110H1"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "0"} 
+    field_option.selected = true
+  end
+
+  def set_G0110I1_pos
+    field = klasses.detect {|x| x.node == "G0110I1"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "0"} 
+    field_option.selected = true
   end
 
 end
