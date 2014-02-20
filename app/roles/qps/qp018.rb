c2bed29c39c80e588224f6e8d8a11418055e4755
pdoise <p.doise@gmail.com>
2014-02-20 12:22:02 -0700

    progress

---
+++ app/roles/qps/qp018.rb
@@ -0,0 +1,45 @@
+module Qps::Qp018
+  #:g0400a, :g0400b
+
+  def apply_requested_qp_attributes(pos_or_neg)
+    if pos_or_neg == "pos"
+      set_G0400A_pos; set_G0400B_pos; 
+    elsif pos_or_neg == "neg"
+      set_G0400A_neg; set_G0400B_neg;
+    end
+  end
+
+  def set_G0400A_pos
+    field = klasses.detect {|x| x.node == "G0400A"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "0"} 
+    field_option.selected = true
+  end
+
+  def set_G0400B_pos
+    field = klasses.detect {|x| x.node == "G0400B"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "0"} 
+    field_option.selected = true
+  end
+
+
+  def set_G0400A_neg
+    field = klasses.detect {|x| x.node == "G0400A"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "2"} 
+    field_option.selected = true
+  end
+
+  def set_G0400B_neg
+    field = klasses.detect {|x| x.node == "G0400B"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "2"} 
+    field_option.selected = true
+  end
+
+end
---
+++ app/roles/qps/qp028b.rb
@@ -0,0 +1,28 @@
+module Qps::Qp028b
+  #:g0110g1, :j1400
+
+  def apply_requested_qp_attributes(pos_or_neg)
+    if pos_or_neg == "pos"
+      set_G0110G1_pos 
+    elsif pos_or_neg == "neg"
+      set_G0110G1_neg
+    end
+  end
+
+  def set_G0110G1_pos
+    field = klasses.detect {|x| x.node == "G0110G1"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "0"} 
+    field_option.selected = true
+  end
+
+  def set_G0110G1_neg
+    field = klasses.detect {|x| x.node == "G0110G1"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "2"} 
+    field_option.selected = true
+  end
+
+end
