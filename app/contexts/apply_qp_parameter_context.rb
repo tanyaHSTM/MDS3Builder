9f6dd2ec15fcf13baeea058e7d0827597c7fb293
pdoise <p.doise@gmail.com>
2014-02-20 09:52:51 -0700

    working qp model

--- app/contexts/apply_qp_parameter_context.rb
+++ app/contexts/apply_qp_parameter_context.rb
@@ -1,26 +1,26 @@
 class ApplyQpParameterContext
-  attr_accessor :assessment, :qp_name
+  attr_accessor :assessment, :qp_name, :pos_or_neg
 
-  def self.call(assessment, qp_name)
-    ApplyQpParameterContext.new(assessment, qp_name).call
+  def self.call(assessment, qp_name, pos_or_neg)
+    ApplyQpParameterContext.new(assessment, qp_name, pos_or_neg).call
   end
 
-  def initialize(assessment, qp_name)
-    @assessment, @qp_name = assessment, qp_name
+  def initialize(assessment, qp_name, pos_or_neg)
+    @assessment, @qp_name, @pos_or_neg = assessment, qp_name, pos_or_neg
 
     @assessment.extend Qps::AttributeSetter
-    @assessment.extend qp_primary_attribute_module(@qp_name)
+    @assessment.extend qp_primary_attribute_module(@qp_name, @pos_or_neg)
   end
 
   def call
     return unless @qp_name.present?
     @assessment.set_all_to_nil
-    @assessment.apply_requested_primary_qp_attributes
+    @assessment.apply_requested_primary_qp_attributes(@pos_or_neg)
   end
 
   private
 
-  def qp_primary_attribute_module(qp_name)
+  def qp_primary_attribute_module(qp_name, pos_or_neg)
     "Qps::#{@qp_name}".constantize
   end
 
--- app/roles/qps/qp017.rb
+++ app/roles/qps/qp017.rb
@@ -1,11 +1,12 @@
 module Qps::Qp017
-  #:g0110a1, :g0110b1, :g0110h1, :g0110i1]
+  #:b0100, :g0110a1, :g0110b1, :g0110h1, :g0110i1
 
-  def apply_requested_primary_qp_attributes
-    set_G0110A1_pos
-    set_G0110B1_pos
-    set_G0110H1_pos
-    set_G0110I1_pos
+  def apply_requested_primary_qp_attributes(pos_or_neg)
+    if pos_or_neg == "pos"
+      set_G0110A1_pos; set_G0110B1_pos; set_G0110H1_pos; set_G0110I1_pos;
+    elsif pos_or_neg == "neg"
+      set_G0110A1_neg; set_G0110B1_neg; set_G0110H1_neg; set_G0110I1_neg;
+    end
   end
 
   def set_G0110A1_pos
@@ -36,7 +37,39 @@ module Qps::Qp017
     field = klasses.detect {|x| x.node == "G0110I1"}
     nilled_option = field.options.detect{|x| x.value == "^"}
     nilled_option.selected = false
-    field_option = field.options.detect{|x| x.value == "0"} 
+    field_option = field.options.detect{|x| x.value == "2"} 
+    field_option.selected = true
+  end
+
+  def set_G0110A1_neg
+    field = klasses.detect {|x| x.node == "G0110A1"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "2"} 
+    field_option.selected = true
+  end
+
+  def set_G0110B1_neg
+    field = klasses.detect {|x| x.node == "G0110B1"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "2"} 
+    field_option.selected = true
+  end
+
+  def set_G0110H1_neg
+    field = klasses.detect {|x| x.node == "G0110H1"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "2"} 
+    field_option.selected = true
+  end
+
+  def set_G0110I1_neg
+    field = klasses.detect {|x| x.node == "G0110I1"}
+    nilled_option = field.options.detect{|x| x.value == "^"}
+    nilled_option.selected = false
+    field_option = field.options.detect{|x| x.value == "2"} 
     field_option.selected = true
   end
 
