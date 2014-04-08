1fc671fb66094f371ff2e37346358a1942a44a92
Phil Doise <p.doise@gmail.com>
2014-04-08 11:36:57 -0600

    added A0100b to inactivations

--- app/models/inactivation_of_entry.rb
+++ app/models/inactivation_of_entry.rb
@@ -5,9 +5,9 @@ class InactivationOfEntry < Assessment
   def title
   	"Inactivation of Entry"
   end
-  
+
   def fields
-    %w( ItmSbstCd StateCd FacId A0050 X0150 X0200a X0200c X0300 X0400 X0500 X0600a X0600b X0600c X0600d X0600f X0700c )
+    %w( ItmSbstCd StateCd FacId A0050 A0100b X0150 X0200a X0200c X0300 X0400 X0500 X0600a X0600b X0600c X0600d X0600f X0700c )
   end
 
 end
