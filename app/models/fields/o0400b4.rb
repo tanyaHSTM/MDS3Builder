e0e6cb9eab9515a076132ead4dab89bb5cd0bca0
Phil Doise <p.doise@gmail.com>
2014-08-07 14:25:55 -0600

    vaccines and admitted from

--- app/models/fields/o0400b4.rb
+++ app/models/fields/o0400b4.rb
@@ -1,12 +1,11 @@
 class O0400b4
-  attr_reader :title, :options, :name, :field_type, :node
+  attr_reader :options, :name, :field_type, :node
 
   def initialize
-    @title = "Special Treaments, Procedures and Programs"
-    @name = "Occupational Therapy - Number of days this therapy was administered for at least 15 minutes 
+    @name = "Occupational Therapy - Number of days this therapy was administered for at least 15 minutes
              a day in the last 7 days (O0400b4)"
     @field_type = DROPDOWN
-    @node = "O0400B4"     
+    @node = "O0400B4"
 
     @options = []
     @options << FieldOption.new("^", "NA")
@@ -23,5 +22,5 @@ class O0400b4
   def set_values_for_type(klass)
     return "0"
   end
-  
+
 end
