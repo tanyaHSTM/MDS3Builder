5a062db882988a868d2091e159970fa542a870c5
Phillip Doise <p.doise@gmail.com>
2015-05-22 18:16:37 -0600

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ MDS3Builder::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.assets.js_compressor = Uglifier.new(mangle: false)
+
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
