24670822e305dfc9eff353abb2f55075357d0e45
Phillip Doise <p.doise@gmail.com>
2015-06-08 15:36:37 -0600

    file export working

--- app/assets/javascripts/assessments/admission_assessment/admissionAssessmentController.js
+++ app/assets/javascripts/assessments/admission_assessment/admissionAssessmentController.js
@@ -4,9 +4,18 @@ angular.module('Mds3Builder')
   $scope.assessment = {};
   $scope.assessment.ITM_SBST_CD = "NC";
 
-  $scope.showJson = function() {
-    $scope.xml = json2xml($scope.assessment, "BASE");
+  $scope.viewXml = function() {
+    $scope.xml_view = json2xml($scope.assessment, "ASSESSMENT");
+    $scope.xml_view = vkbeautify.xml($scope.xml_view);
+  }
+
+  $scope.exportXml = function() {
+    $scope.xml = json2xml($scope.assessment, "ASSESSMENT");
     $scope.xml = vkbeautify.xml($scope.xml);
+    var blob = new Blob(['<?xml version="1.0" encoding="UTF-8"?>\n' + $scope.xml], {
+        type: "application/xml;charset=utf-8"
+    });
+    saveAs(blob, "Admission Assessment.xml");
   }
 
   var json2xml = (function (undefined) {
