492be5bdda15044a8f03bf6ee647092df5e31603
Becca Harris <bharris@providigm.com>
2017-10-17 13:07:00 -0600

    adding a0310h to all inactivations

--- app/models/inactivation_of_death.rb
+++ app/models/inactivation_of_death.rb
@@ -7,7 +7,7 @@ class InactivationOfDeath < Assessment
   end
 
   def fields
-    %w( ItmSbstCd StateCd FacId A0050 A0100b X0150 X0200a X0200c X0300 X0400 X0500 X0600a X0600b X0600c X0600d X0600f X0600h X0700b X0700c )
+    %w( ItmSbstCd StateCd FacId A0050 A0100b A0310h X0150 X0200a X0200c X0300 X0400 X0500 X0600a X0600b X0600c X0600d X0600f X0600h X0700b X0700c )
   end
 
 end
--- app/models/inactivation_of_pps5_day.rb
+++ app/models/inactivation_of_pps5_day.rb
@@ -7,7 +7,7 @@ class InactivationOfPps5Day < Assessment
   end
 
   def fields
-    %w( ItmSbstCd StateCd FacId A0050 A0100b X0150 X0200a X0200c X0300 X0400 X0500 X0600a X0600b X0600c X0600d X0600f X0600h X0700a X0700c )
+    %w( ItmSbstCd StateCd FacId A0050 A0100b A0310h X0150 X0200a X0200c X0300 X0400 X0500 X0600a X0600b X0600c X0600d X0600f X0600h X0700a X0700c )
   end
 
 end
