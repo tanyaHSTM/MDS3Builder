2cce94b13f98896dadadccc9530f3df124dd0eaa
Becca Harris <bharris@providigm.com>
2018-01-03 09:00:54 -0700

    making assessment reference dates match for correction and inactivations

--- app/models/fields/x0700a.rb
+++ app/models/fields/x0700a.rb
@@ -11,8 +11,27 @@ class X0700a
   end
 
   def set_values_for_type(klass)
-    date = Date.today - 10.days
-    return date.strftime("%Y%m%d")
+    date1 = Date.today - 20.days
+    date2 = Date.today - 19.days
+    date3 = Date.today - 18.days
+    date4 = Date.today - 17.days
+    date5 = Date.today - 28.days
+    date6 = Date.today - 15.days
+    date7 = Date.today - 14.days
+    date8 = Date.today - 13.days
+    date9 = Date.today - 12.days
+    case klass
+    when "InactivationOfAdmission" then return date1.strftime("%Y%m%d")
+    when "InactivationOfQuarterly" then return date2.strftime("%Y%m%d")
+    when "InactivationOfAnnual" then return date3.strftime("%Y%m%d")
+    when "InactivationOfSignificantChange" then return date4.strftime("%Y%m%d")
+    when "InactivationOfPps5Day" then return date5.strftime("%Y%m%d")
+    when "InactivationOfPps14Day" then return date6.strftime("%Y%m%d")
+    when "InactivationOfPps30Day" then return date7.strftime("%Y%m%d")
+    when "InactivationOfPps60Day" then return date8.strftime("%Y%m%d")
+    when "InactivationOfPps90Day" then return date9.strftime("%Y%m%d")
+    else return date2.strftime("%Y%m%d")
+    end
   end
 
 end
